f3014f6bfa3746efa22bda50d7557566b0334cd8
Ruian-Li <ruian.li98@gmail.com>
2019-07-21 19:38:27 -0400

    updated user.go

--- service/user.go
+++ service/user.go
@@ -1,161 +1,165 @@
 package main
 
- import (
- 	"encoding/json"
- 	"fmt"
- 	"net/http"
- 	"reflect"
- 	"time"
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"reflect"
+	"regexp"
+	"time"
 
- 	elastic "gopkg.in/olivere/elastic.v3"
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/olivere/elastic"
+)
 
- 	jwt "github.com/dgrijalva/jwt-go"
- 	jwtmiddleware "github.com/auth0/go-jwt-middleware"
- )
+const (
+	USER_INDEX = "user"
+	USER_TYPE  = "user"
+)
 
- const (
- 	TYPE_USER = "user"
- )
-
- type User struct {
- 	Username string `json:"username"`
- 	Password string `json:"password"`
- }
-
- //checks whether user is valid
- func checkUser(username, password string) bool {
- 	es_client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
- 	if err != nil {
- 		fmt.Printf("ES is not setup %v\n", err)
- 		return false
- 	}
-
- 	// Search with a term query
- 	termQuery := elastic.NewTermQuery("username", username)
- 	queryResult, err := es_client.Search().
- 		Index(INDEX).
- 		Query(termQuery).
- 		Pretty(true).
- 		Do()
- 	if err != nil {
- 		fmt.Printf("ES query failed %v\n", err)
- 		return false
- 	}
-
- 	var tyu User
- 	for _, item := range queryResult.Each(reflect.TypeOf(tyu)) {
- 		u := item.(User)
- 		return u.Password == password && u.Username == username
- 	}
- 	// If no user exist, return false.
- 	return false
- }
-
- // Add a new user. Return true if successfully.
- func addUser(username, password string) bool {
- 	// In theory, BigTable is a better option for storing user credentials than ES. However,
- 	// since BT is more expensive than ES so usually students will disable BT.
- 	es_client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
- 	if err != nil {
- 		fmt.Printf("ES is not setup %v\n", err)
- 		return false
- 	}
-
- 	user := &User{
- 		Username: username,
- 		Password: password,
- 	}
-
- 	// Search with a term query
- 	termQuery := elastic.NewTermQuery("username", username)
- 	queryResult, err := es_client.Search().
- 		Index(INDEX).
- 		Query(termQuery).
- 		Pretty(true).
- 		Do()
- 	if err != nil {
- 		fmt.Printf("ES query failed %v\n", err)
- 		return false
- 	}
-
- 	if queryResult.TotalHits() > 0 {
- 		fmt.Printf("User %s has existed, cannot create duplicate user.\n", username)
- 		return false
- 	}
-
- 	// Save it to index
- 	_, err = es_client.Index().
- 		Index(INDEX).
- 		Type(TYPE_USER).
- 		Id(username).
- 		BodyJson(user).
- 		Refresh(true).
- 		Do()
- 	if err != nil {
- 		fmt.Printf("ES save failed %v\n", err)
- 		return false
- 	}
- 	return true
- }
-
-// If signup is successful, a new session is created.
-func signupHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Received one signup request")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Content-Type", "text/plain")
-
-	decoder := json.NewDecoder(r.Body)
-	var u User
-	if err := decoder.Decode(&u); err != nil {
-		m := fmt.Sprintf("Failed to parse body %v", r.Body)
-		fmt.Println(m)
-		http.Error(w, m, http.StatusBadRequest)
-		return
-	}
-
-	if u.Username != "" && u.Password != "" {
-		if addUser(u.Username, u.Password) {
-			fmt.Println("User added successfully.")
-			w.Write([]byte("User added successfully."))
-		} else {
-			fmt.Println("Failed to add a new user.")
-			http.Error(w, "Failed to add a new user", http.StatusInternalServerError)
-		}
-	} else {
-		fmt.Println("Empty password or username.")
-		http.Error(w, "Empty password or username", http.StatusInternalServerError)
-	}
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Age      int64  `json:"age"`
+	Gender   string `json:"gender"`
 }
 
-// If login is successful, a new token is created.
-func loginHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Received one login request ")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+var mySigningKey = []byte("secret")
+
+func checkUser(username, password string) error {
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		return err
+	}
+
+	// select * from users where username = ?
+	query := elastic.NewTermQuery("username", username)
+
+	searchResult, err := client.Search().
+		Index(USER_INDEX).
+		Query(query).
+		Pretty(true).
+		Do(context.Background())
+	if err != nil {
+		return err
+	}
+
+	var utyp User
+	for _, item := range searchResult.Each(reflect.TypeOf(utyp)) {
+		if u, ok := item.(User); ok {
+			if username == u.Username && password == u.Password {
+				fmt.Printf("Login in as %s\n", username)
+				return nil
+			}
+		}
+	}
+
+	return errors.New("Wrong username or password")
+}
+
+func addUser(user User) error {
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		return err
+	}
+
+	// select * from users where username = ?
+	query := elastic.NewTermQuery("username", user.Username)
+
+	searchResult, err := client.Search().
+		Index(USER_INDEX).
+		Query(query).
+		Pretty(true).
+		Do(context.Background())
+	if err != nil {
+		return err
+	}
+
+	if searchResult.TotalHits() > 0 {
+		return errors.New("User already exists")
+	}
+
+	_, err = client.Index().
+		Index(USER_INDEX).
+		Type(USER_TYPE).
+		Id(user.Username).
+		BodyJson(user).
+		Refresh("wait_for").
+		Do(context.Background())
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("User is added: %s\n", user.Username)
+	return nil
+}
+
+func handlerLogin(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one login request")
 	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	decoder := json.NewDecoder(r.Body)
-	var u User
-	if err := decoder.Decode(&u); err != nil {
-		m := fmt.Sprintf("Failed to parse body %v", r.Body)
-		fmt.Println(m)
-		http.Error(w, m, http.StatusBadRequest)
+	var user User
+	if err := decoder.Decode(&user); err != nil {
+		http.Error(w, "Failed to parse JSON input from client", http.StatusBadRequest)
+		fmt.Printf("Failed to parse JSON input from client %v.\n", err)
 		return
 	}
 
-	if checkUser(u.Username, u.Password) {
-		token := jwt.New(jwt.SigningMethodHS256)
-		claims := token.Claims.(jwt.MapClaims)
-		/* Set token claims */
-		claims["username"] = u.Username
-		claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
-
-		/* Sign the token with our secret */
-		tokenString, _ := token.SignedString(mySigningKey)
-
-		/* Finally, write the token to the browser window */
-		w.Write([]byte(tokenString))
-	} else {
-		fmt.Println("Invalid password or username.")
-		http.Error(w, "Invalid password or username", http.StatusForbidden)
+	if err := checkUser(user.Username, user.Password); err != nil {
+		if err.Error() == "Wrong username or password" {
+			http.Error(w, "Wrong username or password", http.StatusUnauthorized)
+		} else {
+			http.Error(w, "Failed to read from ElasticSearch", http.StatusInternalServerError)
+		}
+		return
 	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": user.Username,
+		"exp":      time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	tokenString, err := token.SignedString(mySigningKey)
+	if err != nil {
+		http.Error(w, "Failed to generate token", http.StatusInternalServerError)
+		fmt.Printf("Failed to generate token %v.\n", err)
+		return
+	}
+
+	w.Write([]byte(tokenString))
+}
+
+func handlerSignup(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one signup request")
+	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	decoder := json.NewDecoder(r.Body)
+	var user User
+	if err := decoder.Decode(&user); err != nil {
+		http.Error(w, "Failed to parse JSON input from client", http.StatusBadRequest)
+		fmt.Printf("Failed to parse JSON input from client %v.\n", err)
+		return
+	}
+
+	if user.Username == "" || user.Password == "" || !regexp.MustCompile(`^[a-z0-9_]+$`).MatchString(user.Username) {
+		http.Error(w, "Invalid username or password", http.StatusBadRequest)
+		fmt.Printf("Invalid username or password.\n")
+		return
+	}
+
+	if err := addUser(user); err != nil {
+		if err.Error() == "User already exists" {
+			http.Error(w, "User already exists", http.StatusUnauthorized)
+		} else {
+			http.Error(w, "Failed to save to ElasticSearch", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	w.Write([]byte("User added successfully.\n"))
 }
