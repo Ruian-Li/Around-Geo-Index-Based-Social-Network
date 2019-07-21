0d5adce48c55130382f5d7ceb5f2144042b68204
Ruian-Li <ruian.li98@gmail.com>
2019-07-21 18:53:31 -0400

    implemented saveToBigTable

--- service/main.go
+++ service/main.go
@@ -14,6 +14,7 @@ import (
 	"github.com/pborman/uuid"
 
 	"cloud.google.com/go/storage"
+	"cloud.google.com/go/bigtable"
 )
 
 const (
@@ -161,6 +162,31 @@ func saveToES(p *Post, id string) {
 	}
 
 	fmt.Printf("Post is saved to Index: %s\n", p.Message)
+}
+
+// Save a post to BigTable
+func saveToBigTable(p *Post, id string) {
+	ctx := context.Background()
+	bt_client, err := bigtable.NewClient(ctx, BIGTABLE_PROJECT_ID, BT_INSTANCE)
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	tbl := bt_client.Open("post")
+	mut := bigtable.NewMutation()
+	t := bigtable.Now()
+	mut.Set("post", "user", t, []byte(p.User))
+	mut.Set("post", "message", t, []byte(p.Message))
+	mut.Set("location", "lat", t, []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
+	mut.Set("location", "lon", t, []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
+
+	err = tbl.Apply(ctx, id, mut)
+	if err != nil {
+		panic(err)
+		return
+	}
+	fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
 }
 
 func createIndexIfNotExist() {
